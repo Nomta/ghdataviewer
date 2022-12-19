ba35a9dcacabed4c3b9fc25694582b1a4fcee243
Nomta <nomta@outlook.com>
2022-12-20 00:24:47 +0300

    Intersection Observer

---
+++ client/src/components/IntersectionObserver.vue
@@ -0,0 +1,42 @@
+<template>
+  <slot />
+  <div ref="root"></div>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+export default {
+  name: 'IntersectionObserver',
+
+  props: {
+    options: {
+      type: Object,
+      default: null
+    },
+  },
+
+  emits: ['intersect'],
+
+  setup(props, { emit }) {
+    const root = ref(null)
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry && entry.isIntersecting) {
+        emit('intersect')
+      }
+    }, props.options)
+
+    onMounted(() => {
+      observer.observe(root.value)
+    })
+
+    onUnmounted(() => {
+      observer.disconnect()
+    })
+
+    return {
+      root
+    }
+  }
+}
+</script>
