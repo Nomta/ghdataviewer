8cdf0f051602280844fb81e9b0f7dc5bf5e0e2ff
Nomta <nomta@outlook.com>
2023-05-21 19:46:09 +0300

    ui input

---
+++ client/src/modules/shared/components/ui/UiInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <VTextField v-model="value" :label="label" density="compact" hide-details variant="solo" />
+</template>
+
+<script>
+import { useModel } from '@/modules/shared/composables/useModel'
+
+export default {
+  name: 'UiInput',
+
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    },
+    label: String,
+  },
+
+  emits: ['update:modelValue'],
+
+  setup(props, { emit }) {
+    const value = useModel(props, emit)
+
+    return { value }
+  }
+}
+</script>
+
+<style scoped></style>
