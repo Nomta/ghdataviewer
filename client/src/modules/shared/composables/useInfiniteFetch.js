ccaa659c02182759072182feb8462dbda7cba8ab
Nomta <nomta@outlook.com>
2023-05-28 01:55:36 +0300

    fetch with mapping data

--- client/src/modules/shared/composables/useInfiniteFetch.js
+++ client/src/modules/shared/composables/useInfiniteFetch.js
@@ -1,6 +1,6 @@
 import { ref } from 'vue'
 
-export function useInfiniteFetch(fetcher) {
+export function useInfiniteFetch(fetcher, mapData) {
   const data = ref(null)
   const loading = ref(false)
   const error = ref(null)
@@ -15,7 +15,11 @@ export function useInfiniteFetch(fetcher) {
     return fetcher.next()
       .then(response => {
         if (response.value) {
-          data.value = (data.value || []).concat(response.value)
+          const responseValue = mapData
+            ? mapData(response.value)
+            : response.value
+
+          data.value = (data.value || []).concat(responseValue)
         }
       })
       .catch(e => error.value = e)
